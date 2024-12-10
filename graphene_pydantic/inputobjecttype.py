877341483c6931ca2f9a33789ce71ca23926ff91
Rami Chowdhury <rami.chowdhury@gmail.com>
2024-12-09 23:02:32 -0500

    Remove argument

--- graphene_pydantic/inputobjecttype.py
+++ graphene_pydantic/inputobjecttype.py
@@ -80,8 +80,8 @@ class PydanticInputObjectType(graphene.InputObjectType):
         _meta=None,
         **options,
     ):
-        assert model and issubclass(
-            model, pydantic.BaseModel
+        assert (
+            model and issubclass(model, pydantic.BaseModel)
         ), f'You need to pass a valid Pydantic model in {cls.__name__}.Meta, received "{model}"'
 
         assert isinstance(
@@ -149,8 +149,6 @@ class PydanticInputObjectType(graphene.InputObjectType):
                     model=target_type.model,
                 )
                 fields_to_update[name] = graphene_field
-                meta.registry.register_object_field(
-                    cls, name, pydantic_field, model=target_type.model
-                )
+                meta.registry.register_object_field(cls, name, pydantic_field)
         # update the graphene side of things
         meta.fields.update(fields_to_update)
